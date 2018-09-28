eae84022c3cd7e445c9adc700c9a1aef20f31ae4
Job Kamara <kamarajob@siu.edu>
2018-09-28 09:39:55 -0500

    hide environment variables

---
+++ config/initializers/gibbon.rb
@@ -0,0 +1,6 @@
+
+require 'dotenv/load'
+Gibbon::Request.api_key = ENV["MAILCHIMP_API_KEY"]
+Gibbon::Request.timeout = 15
+Gibbon::Request.throws_exceptions = true
+puts "MailChimp API key: #{Gibbon::Request.api_key}" # temporary
